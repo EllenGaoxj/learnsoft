1ab3b37845d41ba381a9a31694f264db6b923dd4
EllenGaoxj <yolanda_gxj@163.com>
2025-05-25 14:29:22 +0800

    add a new file

---
+++ testcast.py
@@ -0,0 +1,11 @@
+def sumfunc(x,y):
+    z = x + y
+    return z
+def multfunc(x,y):
+    z = x * y
+    return z
+print("this is a test")
+sum = sumfunc(3,4)
+print("This is sum:", sum)
+multnum = multfunc(3,4)
+print("This is multnum:", multnum)
